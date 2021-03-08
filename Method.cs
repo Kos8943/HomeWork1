9d27d96a419378a367cd675357ab817960bf5a96
Kos8943 <a26095998@gmail.com>
2021-03-08 17:18:12 +0800

    加入專案檔案。

---
+++ Method.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace HomeWork1
+{
+    class Method
+    {
+        public static void MoveFile(string fromPath, string toPath)
+        {
+            //來源路徑沒有目標檔案時
+            if(!File.Exists(fromPath))
+            {
+                Console.WriteLine($"檔案來源資料夾無此檔案: {fromPath}");
+                return;
+            }
+
+
+            //請user確認是否執行
+            Console.WriteLine($"是否將{fromPath}的檔案,搬移至{toPath}.請輸入: Y / N");
+            string yes = Console.ReadLine();
+
+
+            //如果目標路徑已有相同檔案名稱時
+            if(File.Exists(toPath))
+            {
+                Console.WriteLine("目標資料夾已存在相同檔案");
+                return;
+            }
+
+
+            //user是否輸入Yes時
+            //yes:執行程式
+            //不是yes:結束程式
+            if(yes == "y" || yes == "Y" || yes == "Yes" || yes == "yes" || yes == "YES")
+            {
+                //開始計時程式執行時間
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+
+                //搬移檔案
+                File.Move(fromPath, toPath);
+
+                //停止計時
+                sw.Stop();
+                Console.WriteLine($"檔案搬移成功! 共使用{sw.ElapsedMilliseconds}秒");
+            }
+            else
+            {
+                return;
+            }
+
+            
+        }
+
+        public static void CopyFile(string fromPath, string toPath)
+        {
+            //來源路徑沒有目標檔案時
+            if (!File.Exists(fromPath))
+            {
+                Console.WriteLine($"檔案來源資料夾無此檔案: {fromPath}");
+                return;
+            }
+
+
+            //如果目標路徑已有相同檔案名稱時
+            if (File.Exists(toPath))
+            {
+                Console.WriteLine("目標資料夾已存在相同檔案");
+                return;
+            }
+
+
+            //請user確認是否執行
+            Console.WriteLine($"是否將{fromPath}的檔案,複製至{toPath}.請輸入: Y / N");
+            string yes = Console.ReadLine();
+
+
+            //user是否輸入Yes時
+            //yes:執行程式
+            //不是yes:結束程式
+            if (yes == "y" || yes == "Y" || yes == "Yes" || yes == "yes" || yes == "YES")
+            {
+                //開始計時程式執行時間
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+
+                //複製檔案
+                File.Copy(fromPath, toPath);
+
+                //停止計時
+                sw.Stop();
+                Console.WriteLine($"檔案搬移成功! 共使用{sw.ElapsedMilliseconds}秒");
+            }
+            else
+            {
+                return;
+            }
+
+
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //提示user程式已啟動
+            Console.WriteLine("---程式啟動---");
+
+            
+            //用陣列args[0]的決定啟動的method
+            string method = args[0];
+
+
+            //將method的字轉成小寫
+            string stringCpmpare = method.ToLower();
+
+
+            //將參數args[1],args[2]設成path1,path2
+            string path1 = args[1];
+            string path2 = args[2];
+            
+
+            //變數method為"MoveFile"時使用此方法
+            if (stringCpmpare == "movefile")
+            {
+                Method.MoveFile(path1, path2);
+            }
+
+
+            //變數method為"CopyFile"時使用此方法
+            if (stringCpmpare == "copyfile") 
+            {
+                Method.CopyFile(path1, path2);
+            }
+
+            Console.WriteLine("---程式結束---");
+        }
+    }
+}
